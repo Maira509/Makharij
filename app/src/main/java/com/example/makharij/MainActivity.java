407144bff1a266bb6c14ac9b39860f4120087c2a
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-05 16:09:09 +0500

    * Splash Screen implemented * Home Page implemented

--- app/src/main/java/com/example/makharij/MainActivity.java
+++ app/src/main/java/com/example/makharij/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.makharij;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Activity2.class));
+                finish();
+            }
+        }, secondsDelayed * 2000);
     }
 }
