86e0e1e31c3d8127acdc2d5e5c74567136cd6c1d
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-05 17:34:21 +0500

    Practice Page implemented

--- app/src/main/java/com/example/makharij/Activity2.java
+++ app/src/main/java/com/example/makharij/Activity2.java
@@ -2,7 +2,10 @@ package com.example.makharij;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        Button button;
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
+    }
+    public void openActivity3() {
+        Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
     }
 }
