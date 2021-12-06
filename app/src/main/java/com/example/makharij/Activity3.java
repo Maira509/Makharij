8dc8c8dd22c415573a7167da87dd8e27339c0523
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-06 14:13:05 +0500

    Modified Practice Page

--- app/src/main/java/com/example/makharij/Activity3.java
+++ app/src/main/java/com/example/makharij/Activity3.java
@@ -2,13 +2,27 @@ package com.example.makharij;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity3 extends AppCompatActivity {
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+         Button button;
+         button = (Button) findViewById(R.id.button4);
+         button.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent = new Intent(Activity3.this, practice_2.class);
+                 intent.putExtra("btn", button.getId());
+                 startActivity(intent);
+             }
+         });
+
     }
 }
---
+++ app/src/main/java/com/example/makharij/practice_2.java
@@ -0,0 +1,30 @@
+package com.example.makharij;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class practice_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String statement = "End of Throat";
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice2);
+        Intent mIntent = getIntent();
+        int intValue = mIntent.getIntExtra("btn", 0);
+        TextView textView;
+        textView = findViewById(R.id.textView2);
+        ImageView img;
+        img = (ImageView) findViewById(R.id.img1);
+
+        if(intValue == R.id.button4){
+            textView.setText(statement);
+            img.setImageResource(R.drawable.throat1);
+        }
+
+    }
+}
