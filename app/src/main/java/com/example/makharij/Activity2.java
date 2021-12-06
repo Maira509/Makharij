d14cb038892e886dca773680f3ad8c3aea18296b
Maira509 <bsef18a509@pucit.edu.pk>
2021-12-06 13:28:52 +0500

    Added Button to go to Github Repository

--- app/src/main/java/com/example/makharij/Activity2.java
+++ app/src/main/java/com/example/makharij/Activity2.java
@@ -3,6 +3,7 @@ package com.example.makharij;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,17 +14,38 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
-        Button button;
+        Button button, q_button, git_btn;
         button = (Button) findViewById(R.id.button);
+        q_button = (Button) findViewById(R.id.button2);
+        git_btn = (Button) findViewById(R.id.button3);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity3();
             }
         });
+        q_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuiz();
+            }
+        });
+        git_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/Maira509/Makharij");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
     }
     public void openActivity3() {
         Intent intent = new Intent(this, Activity3.class);
+        startActivity(intent);
+    }
+    public void openQuiz() {
+        Intent intent = new Intent(this, quiz.class);
         startActivity(intent);
     }
 }
